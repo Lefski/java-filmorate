155514c9d30b053f6eae2bd4f1fb44840c62e1eb
Lefski <93370391+Lefski@users.noreply.github.com>
2023-07-07 02:41:52 +0300

    feat: changed style

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidReleaseDateException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidReleaseDateException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class InvalidReleaseDateException extends RuntimeException{
+public class InvalidReleaseDateException extends RuntimeException {
 
     public InvalidReleaseDateException(String message) {
         super(message);
