e01f8221e601755cf1e2bc581e74f678848e92a9
Lefski <93370391+Lefski@users.noreply.github.com>
2023-07-06 18:22:19 +0300

    feat: changed style

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidBirthdayException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/InvalidBirthdayException.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class InvalidBirthdayException extends RuntimeException{
-    InvalidBirthdayException (String message, Exception e){
+public class InvalidBirthdayException extends RuntimeException {
+    InvalidBirthdayException(String message, Exception e) {
         super(message, e);
     }
 
-    public InvalidBirthdayException(String message){
+    public InvalidBirthdayException(String message) {
         super(message);
     }
 }
