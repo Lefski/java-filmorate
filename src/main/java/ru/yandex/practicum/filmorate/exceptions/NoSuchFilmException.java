9a8d22d34553bc7faef79ae88bb331af93700243
Lefski <93370391+Lefski@users.noreply.github.com>
2023-07-07 02:43:18 +0300

    feat: changed style

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NoSuchFilmException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/NoSuchFilmException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class NoSuchFilmException extends RuntimeException{
-    public NoSuchFilmException(String message){
+public class NoSuchFilmException extends RuntimeException {
+    public NoSuchFilmException(String message) {
         super(message);
     }
 }
