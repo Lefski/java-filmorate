614317e0d0f70a1e360e41c18a8ff90a54477cbe
Lefski <93370391+Lefski@users.noreply.github.com>
2023-07-12 13:47:39 +0300

    fix: applied review changes to MpaController and MpaService

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.dao.daoimpl.MpaDaoImpl;
+import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,18 +19,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MpaController {
 
-    private final MpaDaoImpl mpaDao;
+    private final MpaService mpaService;
 
     @GetMapping
     public List<Mpa> getMpa() {
         log.info("Получен запроc на получение списка всех MPA");
-        return mpaDao.getMPAs();
+        return mpaService.getMpa();
     }
 
     @GetMapping("/{id}")
     public Optional<Mpa> getMpaById(@PathVariable int id) {
         log.info("Получен запроc на поиск MPA по id");
-        return mpaDao.getMpaById(id);
+        return mpaService.getMpaById(id);
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import ru.yandex.practicum.filmorate.exception.NoSuchUserException;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.HashSet;
 
 @Data
